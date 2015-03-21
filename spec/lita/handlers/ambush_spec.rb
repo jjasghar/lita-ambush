84ec448d582b923f637707273d34b849a22e17b5
JJ Asghar <jj@getchef.com>
2015-03-21 11:20:41 -0500

    needed to fix tests

--- spec/lita/handlers/ambush_spec.rb
+++ spec/lita/handlers/ambush_spec.rb
@@ -7,7 +7,7 @@ describe Lita::Handlers::Ambush, lita_handler: true do
 
   it "testing the storage into redis" do
     user1 = Lita::User.create(123, name: "user1")
-    send_command("ambush: user1: this is a taco!", as: user1)
+    send_command("ambush user1: this is a taco!", as: user1)
     expect(replies.last).to eq("#{user1.name} I've stored the ambush")
     redis_rpop=Lita.redis.rpop("handlers:ambush:#{user1.name}")
     outputted_yaml=YAML.load(redis_rpop)
@@ -17,8 +17,8 @@ describe Lita::Handlers::Ambush, lita_handler: true do
 
   it "should send store two messages" do
     user1 = Lita::User.create(123, name: "user1")
-    send_command("ambush: user1: this is a taco!", as: user1)
-    send_command("ambush: user1: this is a burruto!", as: user1)
+    send_command("ambush user1: this is a taco!", as: user1)
+    send_command("ambush user1: this is a burruto!", as: user1)
     expect(replies.last).to eq("#{user1.name} I've stored the ambush")
     expect(replies.last).to eq("#{user1.name} I've stored the ambush")
     redis_rpop=Lita.redis.rpop("handlers:ambush:#{user1.name}")
@@ -37,7 +37,7 @@ describe Lita::Handlers::Ambush, lita_handler: true do
   it "should respond with the message stored" do
     user1 = Lita::User.create(123, name: "user1")
     user2 = Lita::User.create(456, name: "user2")
-    send_command("ambush: #{user2.name}: this is a taco!", as: user1)
+    send_command("ambush #{user2.name}: this is a taco!", as: user1)
     send_message("I like to watch Sulvester Stalone!", as: user2)
     expect(replies.last).to eq("#{user2.name}: While you were out, #{user1.name} said: this is a taco!")
   end
@@ -52,8 +52,8 @@ describe Lita::Handlers::Ambush, lita_handler: true do
   it "should respond with the multiple messages stored" do
     user1 = Lita::User.create(123, name: "user1")
     user2 = Lita::User.create(456, name: "user2")
-    send_command("ambush: #{user2.name}: this is a taco!", as: user1)
-    send_command("ambush: #{user2.name}: this is a burrito!", as: user1)
+    send_command("ambush #{user2.name}: this is a taco!", as: user1)
+    send_command("ambush #{user2.name}: this is a burrito!", as: user1)
     send_message("I like to watch Sulvester Stalone!", as: user2)
     expect(replies[2]).to eq("#{user2.name}: While you were out, #{user1.name} said: this is a taco!")
     expect(replies[3]).to eq("#{user2.name}: While you were out, #{user1.name} said: this is a burrito!")
